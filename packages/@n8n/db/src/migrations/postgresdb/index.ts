e5d92745be166df43888ba378938958d3a032b03
lamtt <lamtt@basebs.com>
2025-10-27 08:59:25 +0700

    fix conflict

--- packages/@n8n/db/src/migrations/postgresdb/index.ts
+++ packages/@n8n/db/src/migrations/postgresdb/index.ts
@@ -42,6 +42,7 @@ import { SeparateExecutionData1690000000020 } from './1690000000020-SeparateExec
 import { AddMissingPrimaryKeyOnExecutionData1690787606731 } from './1690787606731-AddMissingPrimaryKeyOnExecutionData';
 import { MigrateToTimestampTz1694091729095 } from './1694091729095-MigrateToTimestampTz';
 import { CreateTenantId1708421225232 } from './1708421225232-CreateTenant';
+import { CreateTenantId1708421225232 } from './1708421225232-CreateTenant';
 import { AddActivatedAtUserSetting1717498465931 } from './1717498465931-AddActivatedAtUserSetting';
 import { FixExecutionMetadataSequence1721377157740 } from './1721377157740-FixExecutionMetadataSequence';
 import { MigrateTestDefinitionKeyToString1731582748663 } from './1731582748663-MigrateTestDefinitionKeyToString';
@@ -97,11 +98,7 @@ export const postgresMigrations: Migration[] = [
 	InitialMigration1587669153312,
 	WebhookModel1589476000887,
 	CreateIndexStoppedAt1594828256133,
-<<<<<<< HEAD
-	CreateTenantId1652233212434,
-=======
-	CreateTenantId1708421225232,
->>>>>>> main-tenant
+	CreateTenantId1608421225232,
 	AddWebhookId1611144599516,
 	MakeStoppedAtNullable1607431743768,
 	CreateTagEntity1617270242566,
